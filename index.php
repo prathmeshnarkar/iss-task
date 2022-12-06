eb4db0fac55866df525764ea4662aef3486054f0
prathmeshnarkar <78262582+prathmeshnarkar@users.noreply.github.com>
2022-12-06 13:48:28 +0530

    corrected the input parameters for functions

--- index.php
+++ index.php
@@ -1,22 +1,27 @@
 <?php
-$Wb = 360;
-$Hb = 200;
-$Wa = 180;
-$Ha = 250;
 
-print_r(contain($Wb, $Hb, $Wa, $Ha));
+$imageA = ['width'=> 250, 'height'=>500];
+$imageB = ['width'=> 500, 'height'=>90];
 
-function contain($Wb, $Hb, $Wa, $Ha){
+
+print_r(contain($imageA, $imageB));
+
+function contain($imageA, $imageB){
+
+	$Wa = $imageA['width'];
+	$Ha = $imageA['height'];
+	$Wb = $imageB['width'];
+	$Hb = $imageB['height'];
 
 	$Rb = $Wb/$Hb; //ratio for imageB
 
 	$Ra = $Wa/$Ha; //ratio for imageA
 
-	$Wnew = $Ra > $Rb ? ($Wb * $Ha/$Hb) : ($Wa);
+	$Wnew = $Ra > $Rb ? ($Wb * $Ha/$Hb) : ($Wa); //if ratio of imageA is greater then the Width for imageB is calculated WRT the ratio of height
 
-	$Hew = $Ra > $Rb ? ($Ha) : ($Hb * $Wa/$Wb);
+	$Hnew = $Ra > $Rb ? ($Ha) : ($Hb * $Wa/$Wb); //if ratio of imageA is greater then the Height for imageB is calculated WRT the ratio of width
 	
-	return "W : ".$Wnew. " H : ".$Hew;
+	return "W : ".$Wnew. " H : ".$Hnew;
 
 }
 
